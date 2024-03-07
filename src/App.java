8c92f8bd1733aa7b0ceaad799a3713990e9b0723
Matheus Santos <96088072+devmathx@users.noreply.github.com>
2024-03-07 09:19:57 -0300

    text to matrix

--- src/App.java
+++ src/App.java
@@ -1,6 +1,28 @@
+import java.util.ArrayList;
+
+import utils.FileTXT;
+
 class App {
 
+  private static FileTXT txt = new FileTXT("map");
+
   public static void main(String[] args) {
+    transformIntoMatrix();
+  }
+
+  public static void transformIntoMatrix() {
+    ArrayList<String> lines = txt.read();
     
+    int columns = lines.get(0).length();
+    int rows = lines.size();
+
+    String[][] matrix = new String[rows][columns];
+
+    for (int row = 0; row < rows; row++) {
+      String[] chars = lines.get(row).split("");
+      for (int col = 0; col < chars.length; col++) {
+        matrix[row][col] = chars[col];
+      }
+    }
   }
 }
